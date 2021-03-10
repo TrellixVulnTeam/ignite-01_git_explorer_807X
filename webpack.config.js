75063b44ec113db4c84ad231eba426f120eb9ce1
Wagner Costalonga <wagcostalonga@gmail.com>
2021-03-10 16:05:49 -0300

    secondy commit

--- webpack.config.js
+++ webpack.config.js
@@ -7,17 +7,17 @@ const isDevelopment = process.env.NODE_ENV !== 'production'
 module.exports = {
   mode: isDevelopment ? 'development' : 'production',
   devtool: isDevelopment ? 'eval-source-map' : 'source-map',
-  entry: path.resolve(__dirname, 'src', 'index.jsx'),
+  entry: path.resolve(__dirname, 'src', 'index.tsx'),
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
   },
   resolve: {
-    extensions: ['.js', '.jsx']
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
   },
   devServer: {
     contentBase: path.resolve(__dirname, 'public'),
-    hot: true
+    hot: true,
   },
   plugins: [
     isDevelopment && new ReactRefreshWebpackPlugin(),
@@ -28,10 +28,10 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.jsx$/,
+        test: /\.[jt]sx?$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader',
+          loader: require.resolve('babel-loader'),
           options: {
             plugins: [
               isDevelopment && require.resolve('react-refresh/babel')
